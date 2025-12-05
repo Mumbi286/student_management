1b922ca8d82b7599d83518819d6031d88934ab9b
Christine Mumbi <christinemumbi286@gmail.com>
2025-12-05 06:20:35 +0300

    Adds the comments to the functions

--- student.py
+++ student.py
@@ -1,15 +1,17 @@
 from database import connection
 
+# Adds a student
 def add_student(name, course, mobile):
     conn =  connection()
     cursor = conn.cursor()
     cursor.execute(
-        "" INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)"",
+        "INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
         (name, curse, mobile)
     )
     conn.commit()
     conn.close()
 
+# Update the student record
 def view_student():
     conn = connection()
     cursor = conn.cursor()
@@ -18,6 +20,7 @@ def view_student():
     conn.close()
     return rows
 
+# search if the student exists
 def search_student(keyword):
     conn = connection()
     cursor = conn.cursor()
@@ -25,3 +28,25 @@ def search_student(keyword):
         "SELECT * FROM students WHERE name LIKE ? OR moblie LIKE ?",
         (f"%{keyword}, %{keyword}, %{keyword}")
     )
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+# Update the students records
+def update_student(student_id, name,course, mobile):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE students SET name=?, course=?, mobile=? WHERE id=?",
+        (name, course, mobile, student_id)
+    )
+    conn.commit()
+    conn.close()
+
+# Delete a student record from the database
+def delete_student(student_id):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM students WHERE id=?", (student_id))
+    conn.commit()
+    conn.close()
